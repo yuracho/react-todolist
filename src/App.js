962a50320a87a093ab38779b4ebc4e79fb0811c5
yuracho <74102440+yuracho@users.noreply.github.com>
2021-03-31 10:12:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
 import Todo from "./components/Todo";
-import { createGlobalStyle } from "styled-components";
-import TodoTemplate from "./components/TodoTemplate";
-
-const GlobalStyle = createGlobalStyle`
-body{
-  background: #e9ecef;
-}
-`;
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-
-      {/* <TodoTemplate></TodoTemplate> */}
       <Todo />
     </>
   );
 }
 
 export default App;
+
